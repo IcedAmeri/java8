0682b90a9b207c9883eeaa69b269249a18eeca28
IcedAmeri <seokhyunkho@gmail.com>
2022-02-12 00:01:44 +0900

    [인터페이스의 변화] 자바8 API의 기본 메소드와 스태틱 메소드

--- src/main/java/study/java8to11/_03_interface/App.java
+++ src/main/java/study/java8to11/_03_interface/App.java
@@ -1,5 +1,10 @@
 package study.java8to11._03_interface;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
     public static void main(String[] args) {
         // 기본 메소드
@@ -9,5 +14,28 @@ public class App {
 
         // 스태틱 메소드
         Foo.printAnything();
+
+        // Iterable
+        List<String> name = new ArrayList<>();
+        name.add("keesun");
+        name.add("whiteship");
+        name.add("toby");
+        name.add("foo");
+
+        name.forEach(System.out::println);
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println));
+        System.out.println("===========");
+        while (spliterator1.tryAdvance(System.out::println));
+
+        // removeIf
+        name.removeIf(s -> s.startsWith("k"));
+        name.forEach(System.out::println);
+
+        // comparator
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed());
     }
 }
