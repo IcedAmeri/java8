aa1f7239aed7c8d6594f48b8c530914fa3a4794a
IcedAmeri <seokhyunkho@gmail.com>
2022-02-12 19:12:41 +0900

    [Optional] Optional 소개

---
+++ src/main/java/study/java8to11/_05_Optional/Progress.java
@@ -0,0 +1,18 @@
+package study.java8to11._05_Optional;
+
+import java.time.Duration;
+
+public class Progress {
+
+    private Duration studyDuration;
+
+    private boolean finished;
+
+    public Duration getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Duration studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+}
